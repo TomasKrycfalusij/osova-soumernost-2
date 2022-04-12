acb911ada3d3f330b93342f81a25500e8b5010e8
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-04-12 20:08:11 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 displej = 5
 osaX = 2
-
 #OSA
 def draw_osaX():
     for i in range(displej):
         led.plot_brightness(osaX, i, 100)
 
 #BODY
-A = [2, randint(0, 4)]
+A = 0
+def rozhod_body():
+    global displej, A
+    A = [2, randint(0, displej-1)]
+    B = [randint(0,1), randint(0, displej-1)]
+    C = [randint(0,1), randint(0, displej-1)]
+    if B == C:
+        rozhod_body()
+rozhod_body()
+print(A)
 
-def zobrazeni_bodu():
-    led.plot(A[0], A[1])
+#def zobrazeni_bodu():
+#led.plot(A[0], A[1])
 
 
 
 
 for a in range(3):
     draw_osaX()
-    zobrazeni_bodu()
+    #zobrazeni_bodu()
     basic.pause(1000)
     basic.clear_screen()
     basic.pause(1000)
--- main.ts
+++ main.ts
@@ -8,14 +8,25 @@ function draw_osaX() {
 }
 
 // BODY
-let A = [2, randint(0, 4)]
-function zobrazeni_bodu() {
-    led.plot(A[0], A[1])
+let A = []
+function rozhod_body() {
+    
+    A = [2, randint(0, displej - 1)]
+    let B = [randint(0, 1), randint(0, displej - 1)]
+    let C = [randint(0, 1), randint(0, displej - 1)]
+    if (B == C) {
+        rozhod_body()
+    }
+    
 }
 
+rozhod_body()
+console.log(A)
+// def zobrazeni_bodu():
+// led.plot(A[0], A[1])
 for (let a = 0; a < 3; a++) {
     draw_osaX()
-    zobrazeni_bodu()
+    // zobrazeni_bodu()
     basic.pause(1000)
     basic.clearScreen()
     basic.pause(1000)
